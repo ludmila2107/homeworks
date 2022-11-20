b4c59d912f40420e0dc55ed714818349f005d959
Ludmila <lvminzulina@mail.ru>
2022-11-20 22:28:50 +0300

    3

--- task.js
+++ task.js
@@ -1,26 +1,47 @@
 function cachingDecoratorNew(func) {
   let cache = [];
   const maxCacheValueCount = 5;
-   
+
   return (...args) => {
-  const hash = args.join(",");
-  let objectFromCache = cache.find((item) => item.hash === hash);
-  if (objectFromCache) {
-  console.log("Из кэша: " + objectFromCache.value);
-  return "Из кэша: " + objectFromCache.value;
-  }
-   
-  const value = func(...args);
-  cache.push({ hash, value });
-  if (cache.length > maxCacheValueCount) {
-  cache.shift();
-  }
-  console.log("Вычисляем: " + value);
-  return "Вычисляем: " + value;
+    const hash = args.join(",");
+    let objectFromCache = cache.find((item) => item.hash === hash);
+    if (objectFromCache) {
+      console.log("Из кэша: " + objectFromCache.value);
+      return "Из кэша: " + objectFromCache.value;
+    }
+
+    const value = func(...args);
+    cache.push({
+      hash,
+      value
+    });
+    if (cache.length > maxCacheValueCount) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + value);
+    return "Вычисляем: " + value;
   };
+}
+
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay);
   }
-
-
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return wrapper;
 }
